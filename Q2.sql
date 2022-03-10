29590bf6d231847d173ae915a30566e56a45724f
Prateek Shukla <53486253+Intangible-pg18@users.noreply.github.com>
2022-03-10 09:07:12 +0530

    Create Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -4,40 +4,40 @@ SELECT
   item_id,
   test_a AS test_assignment,
   'test_a' AS test_number,
-  CAST('2021-01-11 00:00:00' AS timestamp) AS dummy_test_start_date
+  CAST('2022-03-08 00:00:00' AS timestamp) AS dummy_test_start_date
 FROM dsv1069.final_assignments_qa
 UNION ALL
 SELECT 
   item_id,
   test_b AS test_assignment,
   'test_b' AS test_number,
-  CAST('2021-01-11 00:00:00' AS timestamp) AS dummy_test_start_date
+  CAST('2022-03-08 00:00:00' AS timestamp) AS dummy_test_start_date
 FROM dsv1069.final_assignments_qa
 UNION ALL
 SELECT 
   item_id,
  test_c AS test_assignment,
  'test_c' AS test_number,
- CAST('2021-01-10 00:00:00' AS timestamp) AS dummy_test_start_date
+ CAST('2022-03-08 00:00:00' AS timestamp) AS dummy_test_start_date
 FROM dsv1069.final_assignments_qa
 UNION ALL
 SELECT 
   item_id,
   test_d AS test_assignment,
   'test_d' AS test_number,
-  CAST('2021-01-10 00:00:00' AS timestamp) AS dummy_test_start_date
+  CAST('2022-03-08 00:00:00' AS timestamp) AS dummy_test_start_date
 FROM dsv1069.final_assignments_qa
 UNION ALL
 SELECT
   item_id,
   test_e AS test_assignment,
   'test_e' AS test_number,
-  CAST('2021-01-10 00:00:00' AS timestamp) AS dummy_test_start_date
+  CAST('2022-03-08 00:00:00' AS timestamp) AS dummy_test_start_date
 FROM dsv1069.final_assignments_qa
 UNION ALL
 SELECT 
   item_id,
   test_f AS test_assignment,
   'test_f' AS test_number,
-  CAST('2021-01-10 00:00:00' AS timestamp) AS dummy_test_start_date
+  CAST('2022-03-08 00:00:00' AS timestamp) AS dummy_test_start_date
 FROM dsv1069.final_assignments_qa
